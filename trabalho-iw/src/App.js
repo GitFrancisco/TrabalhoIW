c48ba70f99b290086bc7d37ecad3fd641a1fa559
Francisco Pires <franciscop11@outlook.pt>
2023-11-27 12:36:26 +0000

    API

--- trabalho-iw/src/App.js
+++ trabalho-iw/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Axios from "axios";
+import { useState } from "react";
 import Navbar from './components/Navbar';
 
 function App() {
+  const [receita, setReceita] = useState("");
+
+  //Axios.get("https://catfact.ninja/fact").then((res) => {
+  //  setReceita(res.data.fact);
+  //});
+
+  Axios.get("https://api.sheety.co/7a067a08c05347be6ceb18c1b3f63c24/baseDeDadosTrabalhoIw/página1").then((res) => {
+    setReceita(res.data.página1[0].nomeReceita);
+  });
+
   return (
     <div className="App">
-      <Navbar />
+      <p>{receita}</p>
     </div>
   );
 }
