8b005dcb02f42aa94da6b88f348398d693b75216
Francisco Pires <franciscop11@outlook.pt>
2023-12-02 15:01:02 +0000

    Update API

--- trabalho-iw/src/App.js
+++ trabalho-iw/src/App.js
@@ -6,17 +6,39 @@ import Navbar from './components/Navbar';
 function App() {
   const [receita, setReceita] = useState("");
 
-  //Axios.get("https://catfact.ninja/fact").then((res) => {
-  //  setReceita(res.data.fact);
-  //});
+  const fetchData = () => {
+    Axios.get("https://api.sheety.co/7a067a08c05347be6ceb18c1b3f63c24/baseDeDadosTrabalhoIw/página1").then((res) => {
+      setReceita(res.data.página1[2]);
+    });
+  };
 
-  Axios.get("https://api.sheety.co/7a067a08c05347be6ceb18c1b3f63c24/baseDeDadosTrabalhoIw/página1").then((res) => {
-    setReceita(res.data.página1[0].nomeReceita);
-  });
+  const postData = () => {
+    // Aqui você pode criar um objeto com os dados que deseja enviar
+    const dataToPost = {
+      "nomeReceita": "Bolo de Koala",
+      "ingrediente": "Koala",
+      "tempo": "200 segundos",
+      "imagem": "https://c02.purpledshub.com/uploads/sites/62/2022/09/Koala.-GettyImages-1266039701-46f8a64.jpg?w=1029&webp=1",
+      "id" : 10
+    };
+
+    // Use Axios.post para enviar dados
+    Axios.post("https://api.sheety.co/7a067a08c05347be6ceb18c1b3f63c24/baseDeDadosTrabalhoIw/página1", { página1: [dataToPost] })
+      .then((res) => {
+        console.log("Dados postados com sucesso:", res.data);
+        // Atualize o estado com os dados recém-postados, se necessário
+        setReceita(dataToPost);
+      })
+      .catch((error) => {
+        console.error("Erro ao postar dados:", error);
+      });
+  };
 
   return (
     <div className="App">
-      <p>{receita}</p>
+      <button onClick={postData}></button>
+      <h3>{receita.nomeReceita}</h3>
+      <img class="boloImg" src={receita.imagem}/>
     </div>
   );
 }
