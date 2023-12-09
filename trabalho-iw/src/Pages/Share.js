b7b9764f86920ff06e2738875eaf907415512cc4
Francisco Pires <franciscop11@outlook.pt>
2023-12-09 19:15:09 +0000

    Share Page

--- trabalho-iw/src/Pages/Share.js
+++ trabalho-iw/src/Pages/Share.js
@@ -6,8 +6,12 @@ import { useState } from "react";
 
 
 function Share() {
-    const [receita, setReceita] = useState("");
+    const RecipeName = "Your Recipe Name";
+    const Ingredient = "Your Ingredients";
+    const Time = "Your Time";
+    const Image = "";
 
+    const [receita, setReceita] = useState("");
     const [add_nomeReceita, setNomeReceita] = useState("");
     const [add_ingrediente, setIngrediente] = useState("");
     const [add_tempo, setTempo] = useState("");
@@ -47,30 +51,47 @@ function Share() {
   
     return (
       <div className="Share">
-        <label>
-          Nome da Receita: <input type="text" value={add_nomeReceita} onChange= {(e) => setNomeReceita(e.target.value)} name="nomeReceita"/>
-        </label>
-        <hr />
-        <label>
-          Ingrediente: <input type="text" value={add_ingrediente} onChange= {(e) => setIngrediente(e.target.value)} name="nomeReceita"/>
-        </label>
-        <hr />
-        <label>
-          Tempo: <input type="text" value={add_tempo} onChange= {(e) => setTempo(e.target.value)} name="nomeReceita"/>
-        </label>
-        <hr />
-        <label>
-          Imagem: <input type="text" value={add_imagem} onChange= {(e) => setImagem(e.target.value)} name="nomeReceita"/>
-        </label>
-        <hr />
-        <button type="submit" onClick={postData}>Enviar</button>
-        <button type="submit" onClick={fetchData}>Mostrar</button>
-        <h3>{receita.nomeReceita}</h3>
-        <h3>{receita.ingrediente}</h3>
-        <h3>{receita.tempo}</h3>
-        <img class="boloImg" src={receita.imagem}/>
+        <div className="leftSideShare">
+            <h1 className='shareTitle'>Your Recipe</h1>
+            <label className='lbRecipe'>Name: <input type="text" value={add_nomeReceita} onChange= {(e) => setNomeReceita(e.target.value)} name="nomeReceita"/></label>
+            <label className='lbRecipe'>Ingredient: <input type="text" value={add_ingrediente} onChange= {(e) => setIngrediente(e.target.value)} name="nomeReceita"/></label>
+            <label className='lbRecipe'>Time: <input type="text" value={add_tempo} onChange= {(e) => setTempo(e.target.value)} name="nomeReceita"/></label>
+            <label className='lbRecipe'>Image: <input type="text" value={add_imagem} onChange= {(e) => setImagem(e.target.value)} name="nomeReceita"/></label>
+            <button type="submit" onClick={postData}>Submit</button>
+        </div>
+        <div className="rightSideShare">
+            <h1 className='shareTitle'>Recipe Preview</h1>
+            <h3>{RecipeName}</h3>
+            <img className='imgPreview' src={receita.imagem}/>
+            <h3>{Ingredient}</h3>
+            <h3>{Time}</h3>
+        </div>
       </div>)
-  
 }
 
 export default Share;
+
+/*
+<label>
+Nome da Receita: <input type="text" value={add_nomeReceita} onChange= {(e) => setNomeReceita(e.target.value)} name="nomeReceita"/>
+</label>
+<hr />
+<label>
+Ingrediente: <input type="text" value={add_ingrediente} onChange= {(e) => setIngrediente(e.target.value)} name="nomeReceita"/>
+</label>
+<hr />
+<label>
+Tempo: <input type="text" value={add_tempo} onChange= {(e) => setTempo(e.target.value)} name="nomeReceita"/>
+</label>
+<hr />
+<label>
+Imagem: <input type="text" value={add_imagem} onChange= {(e) => setImagem(e.target.value)} name="nomeReceita"/>
+</label>
+<hr />
+<button type="submit" onClick={postData}>Enviar</button>
+<button type="submit" onClick={fetchData}>Mostrar</button>
+<h3>{receita.nomeReceita}</h3>
+<h3>{receita.ingrediente}</h3>
+<h3>{receita.tempo}</h3>
+<img class="boloImg" src={receita.imagem}/>
+*/
