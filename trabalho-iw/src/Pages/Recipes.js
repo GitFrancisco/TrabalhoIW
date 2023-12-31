675b967cf78193884b0b3b18111643b755d682b0
Francisco Pires <franciscop11@outlook.pt>
2023-12-31 15:54:56 +0000

    Added Search Function Recipes

--- trabalho-iw/src/Pages/Recipes.js
+++ trabalho-iw/src/Pages/Recipes.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "../styles/Recipes.css";
-// APIs
 import Axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
-
 
 function Recipes() {
-    const [receitas, setReceitas] = useState([]);
+  const [receitas, setReceitas] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = () => {
-        Axios.get("https://sheetdb.io/api/v1/e0qsyv4qfu64j?sheet=ctgBolos").then((res) => {
-            setReceitas(res.data);
-        });
-    };
-
-    return (
-        <div className="Recipes">
-            
-        </div>
+  const fetchData = () => {
+    Axios.get("https://sheetdb.io/api/v1/e0qsyv4qfu64j?sheet=ctgBolos").then(
+      (res) => {
+        setReceitas(res.data);
+      }
     );
+  };
+
+  const filterRecipes = () => {
+    return receitas.filter((receita) =>
+      receita.nomeReceita.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
+
+  return (
+    <div className="Recipes">
+      <input
+        id="searchBar"
+        placeholder="Search for a specific recipe!"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div>
+        <h2>Receitas:</h2>
+        {filterRecipes().map((receita, index) => (
+          <div key={index} className="receita-item">
+            <p>Nome: {receita.nomeReceita}</p>
+            <img className="imgPreview" src={receita.imagem} alt={`Recipe ${index}`} />
+            <p>Tempo: {receita.tempo}</p>
+            <p>Ingredientes: {receita.ingrediente}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Recipes;
