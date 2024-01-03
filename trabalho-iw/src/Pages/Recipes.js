31f587a1a40e581b1dc9579337e610cdb9bc51cc
Francisco Pires <franciscop11@outlook.pt>
2024-01-03 18:11:18 +0000

    Added Ingredient List

--- trabalho-iw/src/Pages/Recipes.js
+++ trabalho-iw/src/Pages/Recipes.js
@@ -12,9 +12,13 @@ function Recipes() {
   // Full context
   const [recipeName, setRecipeName] = useState("Bolo de Laranja");
   const [ingredient, setIngredient] = useState("Laranja");
-  const [description, setDescription] = useState("Esta é uma deliciosa receita de bolo de laranja.");
+  const [description, setDescription] = useState(
+    "Esta é uma deliciosa receita de bolo de laranja."
+  );
   const [time, setTime] = useState("10 segundos");
-  const [image, setImage] = useState("https://i.ytimg.com/vi/4-yT1rjtdm8/maxresdefault.jpg");
+  const [image, setImage] = useState(
+    "https://i.ytimg.com/vi/4-yT1rjtdm8/maxresdefault.jpg"
+  );
 
   useEffect(() => {
     //  fetchData();
@@ -82,21 +86,30 @@ function Recipes() {
         </div>
       ))}
 
-    {selectedRecipe ? (    
- <div className="fullContext">
-    <div className="contextLeft">
-      <h1 className="textContext">{recipeName}</h1>
-      <p className="textContext">{time}</p>
-      <img src={image}/>
-      <button onClick={() => { setSelectedRecipe("") }}>Close</button>
+      {selectedRecipe ? (
+        <div className="fullContext">
+          <div className="contextLeft">
+            <h1 className="textContext">{recipeName}</h1>
+            <p className="textContext">{time}</p>
+            <img src={image} />
+            <button
+              onClick={() => {
+                setSelectedRecipe("");
+              }}
+            >
+              Close
+            </button>
+          </div>
+          <div className="contextRight">
+            <p className="textContext">{ingredient}</p>
+            <p className="textContext">{description}</p>
+          </div>
+        </div>
+      ) : (
+        <div hidden></div>
+      )}
     </div>
-    <div className="contextRight">
-      <p className="textContext">{ingredient}</p>
-      <p className="textContext">{description}</p>
-    </div>
-</div>) : <div hidden></div>}
-</div>
-  );      
+  );
 }
 
 export default Recipes;
--- trabalho-iw/src/Pages/Share.js
+++ trabalho-iw/src/Pages/Share.js
@@ -1,66 +1,66 @@
 import React from 'react';
 import "../styles/Share.css";
-// APIs
 import Axios from "axios";
 import { useState } from "react";
 
-
-
 function Share() {
     const [add_nomeReceita, setNomeReceita] = useState("Your Recipe Name");
-    const [add_ingrediente, setIngrediente] = useState("Your Ingredients");
+    const [add_ingrediente, setIngrediente] = useState("Ingredient 1, Ingredient 2");
     const [add_tempo, setTempo] = useState("Your Time");
     const [add_descricao, setDescricao] = useState("Your Description");
-    // images
     const [add_imagem, setImagem] = useState("");
-  
+
     const postData = () => {
-      fetch('https://sheetdb.io/api/v1/e0qsyv4qfu64j?sheet=ctgBolos', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            data: [
-                {
-                    'nomeReceita': add_nomeReceita,
-                    'ingrediente': add_ingrediente,
-                    'tempo': add_tempo,
-                    'descricao' : add_descricao,
-                    'imagem':add_imagem
-                }
-            ]
+        fetch('https://sheetdb.io/api/v1/e0qsyv4qfu64j?sheet=ctgBolos', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                data: [
+                    {
+                        'nomeReceita': add_nomeReceita,
+                        'ingrediente': add_ingrediente.split(',').map(item => item.trim()),
+                        'tempo': add_tempo,
+                        'descricao' : add_descricao,
+                        'imagem': add_imagem
+                    }
+                ]
+            })
         })
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     }
-  
+
     return (
-      <div className="Share">
-        <div className="leftSideShare">
-            <h1 className='shareTitle'>Your Recipe</h1>
-            <label className='lbRecipe'>Name: <input type="text" value={add_nomeReceita} onChange= {(e) => setNomeReceita(e.target.value)} name="nomeReceita"/></label>
-            <label className='lbRecipe'>Ingredient: <input type="text" value={add_ingrediente} onChange= {(e) => setIngrediente(e.target.value)} name="nomeReceita"/></label>
-            <label className='lbRecipe'>Time: <input type="text" value={add_tempo} onChange= {(e) => setTempo(e.target.value)} name="nomeReceita"/></label> 
-            <label className='lbRecipe'>Image: <input type="text" value={add_imagem} onChange= {(e) => setImagem(e.target.value)} name="nomeReceita"/></label>
-            <label className='lbRecipe'>Description: <textarea cols="50" rows="10" value={add_descricao} onChange= {(e) => setDescricao(e.target.value)} name="nomeReceita"/></label> 
+        <div className="Share">
+            <div className="leftSideShare">
+                <h1 className='shareTitle'>Your Recipe</h1>
+                <label className='lbRecipe'>Name: <input type="text" value={add_nomeReceita} onChange={(e) => setNomeReceita(e.target.value)} name="nomeReceita"/></label>
+                <label className='lbRecipe'>Ingredients: <input type="text" value={add_ingrediente} onChange={(e) => setIngrediente(e.target.value)} name="nomeReceita"/></label>
+                <label className='lbRecipe'>Time: <input type="text" value={add_tempo} onChange={(e) => setTempo(e.target.value)} name="nomeReceita"/></label>
+                <label className='lbRecipe'>Image: <input type="text" value={add_imagem} onChange={(e) => setImagem(e.target.value)} name="nomeReceita"/></label>
+                <label className='lbRecipe'>Description: <textarea cols="50" rows="10" value={add_descricao} onChange={(e) => setDescricao(e.target.value)} name="nomeReceita"/></label>
 
-            <button id="submitButton" type="submit" onClick={postData}>Submit</button>
+                <button id="submitButton" type="submit" onClick={postData}>Submit</button>
+            </div>
+            <div className="rightSideShare">
+                <h1 className='shareTitle'>Recipe Preview</h1>
+                <h3>{add_nomeReceita}</h3>
+                <img className='imgPreview' src={add_imagem}/>
+                <h3>Ingredients:</h3>
+                <ul>
+                    {add_ingrediente.split(',').map((ingredient, index) => (
+                        <li key={index}>{ingredient.trim()}</li>
+                    ))}
+                </ul>
+                <h3>{add_descricao}</h3>
+                <h3>{add_tempo}</h3>
+            </div>
         </div>
-        <div className="rightSideShare">
-            <h1 className='shareTitle'>Recipe Preview</h1>
-            <h3>{add_nomeReceita}</h3>
-            <img className='imgPreview' src={add_imagem}/>
-            <h3>{add_ingrediente}</h3>
-            <h3>{add_descricao}</h3>
-            <h3>{add_tempo}</h3>
-        </div>
-      </div>)
+    )
 }
-
-
 
 export default Share;
 
